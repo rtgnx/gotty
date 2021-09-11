eeb4740e914b9488bcd3a3e5ca750d3b78e11f0c
rtgnx <adrian@cybulski.cc>
2021-09-11 13:47:48 +0100

    Update shell.sh

--- shell.sh
+++ shell.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
 
-PASSWD_ENTRY="${X_Forwarded_User}:x:${X_Forwarded_Uid}:${X_Forwarded_Gid}:${X_Forwarded_Fullname}:/:/bin/sh"
-
-echo "$PASSWD_ENTRY" >> /etc/passwd
-
-docker run -it --rm -v /etc/passwd:/etc/passwd --user "${X_Forwarded_Uid}" alpine:3.14
+docker run -it --rm -v /home/${X_Forwarded_User}:/home/${X_Forwarded_User} -v /etc/passwd:/etc/passwd -v /etc/group:/etc/group --v /etc/netauth:/etc/netauth:ro --user "${X_Forwarded_Uid}:${X_Forwarded_Uid}" alpine:3.14
